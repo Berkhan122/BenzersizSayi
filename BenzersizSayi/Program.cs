c7d6471ec8a89d37d48bf8907aab8ae4692414bd
Berkhan Tapan <berkhantapan0665@gmail.com>
2021-03-07 14:58:12 +0300

    Deneme

---
+++ BenzersizSayi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenzersizSayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Benzersiz Sayı Örneği
+            Console.Write("Kaç Sayı Girilmesini İstiyorsunuz:");
+            byte sayiadet = byte.Parse(Console.ReadLine());
+            int[] rndarray = new int[sayiadet];
+            Random rnd = new Random();
+
+            for (int i = 0; i < sayiadet; i++)
+            {
+                rndarray[i] = rnd.Next(10, 100);
+            }
+
+            
+        }
+    }
+}
